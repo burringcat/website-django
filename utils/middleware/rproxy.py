a2e02d32f065a347da30be7904a3b2964c243490
wz <wz@0u0.fun>
2020-03-03 15:52:57 +0800

    add some debug info

--- utils/middleware/rproxy.py
+++ utils/middleware/rproxy.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect
+from django.conf import settings
 from utils.utils.rproxy import country_redirect
 from utils.utils.misc import get_country
 from ipware import get_client_ip
@@ -6,6 +7,8 @@ class IPMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
     def process_request(self, request):
+        if settings.DEBUG is True:
+            print('processing IP middlewarex')
         ip, is_routable = get_client_ip(request)
         request.META['IP'] = ip
         request.META['IS_IP_ROUTABLE'] = is_routable
@@ -17,6 +20,8 @@ class CountryMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
     def process_request(self, request):
+        if settings.DEBUG is True:
+            print('processing country middleware')
         if request.META['IS_IP_ROUTABLE']:
             request.META['COUNTRY'] = get_country(request.META['IP'])
     def __call__(self, request):
@@ -28,6 +33,8 @@ class CountryRedirectMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        if settings.DEBUG is True:
+            print('processing country redirect middleware')
         should_redirect, target = country_redirect(request)
         if should_redirect is False:
             response = self.get_response(request)
