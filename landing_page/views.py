944787ecc27a7816efbf9facf1c253f103f8452f
wz <wz@0u0.fun>
2020-03-03 15:58:37 +0800

    add more debug info

--- landing_page/views.py
+++ landing_page/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.conf import settings
 # Create your views here.
 def home(request):
+    if settings.DEBUG is True:
+        print('routing to landing page.')
     return render(request, 'landing_page/index.html')
