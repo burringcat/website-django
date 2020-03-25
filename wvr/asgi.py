dc8e77600fb2ba43880d8eff951d541ff293902e
Abraham Finch <abraham@odinfinch.xyz>
2020-03-25 18:42:44 +0800

    fix asgi environment variables

--- wvr/asgi.py
+++ wvr/asgi.py
@@ -6,7 +6,9 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 from channels.routing import get_default_application
-
+from utils.utils.env import load_env
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wvr.settings")
+load_env()
 django.setup()
 application = get_default_application()
+
